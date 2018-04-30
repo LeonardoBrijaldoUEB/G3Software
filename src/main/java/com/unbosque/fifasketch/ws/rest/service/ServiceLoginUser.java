9573ec55bdde8599dcd962b25af6026cc0d423c3
Leonardo Arturo Brijaldo Calle <lbrijaldo@unbosque.edu.co>
2018-04-30 18:38:09 -0500

    Util

--- src/main/java/com/unbosque/fifasketch/ws/rest/service/ServiceLoginUser.java
+++ src/main/java/com/unbosque/fifasketch/ws/rest/service/ServiceLoginUser.java
@@ -1,4 +1,5 @@
 package com.unbosque.fifasketch.ws.rest.service;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -11,16 +12,16 @@ public class ServiceLoginUser {
 
 	@POST
 	@Path("/validarUsuario")
-	@Consumes({MediaType.APPLICATION_JSON})
-	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
 	public VOUsuario validarUsuario(VOUsuario vo) {
 		vo.setEstadoUsuario(false);
-		if (vo.getUsuario().equals("dranoel") && vo.getPassword().equals("12345")){
+		if (vo.getUsuario().equals("dranoel") && vo.getPassword().equals("12345")) {
 			vo.setEstadoUsuario(true);
 		}
-		
+
 		vo.setPassword("**************");
 		return vo;
 	}
-	
+
 }
